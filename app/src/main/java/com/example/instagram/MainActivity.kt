e2e93e578e20bdabbab02ecd869d69e1428cfc6a
Hongkun Zeng <hzeng21@illinois.edu>
2022-04-02 01:31:34 -0500

    part 2 instagram

--- app/src/main/java/com/example/instagram/MainActivity.kt
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -14,6 +14,11 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.instagram.fragments.ComposeFragment
+import com.example.instagram.fragments.FeedFragment
+import com.example.instagram.fragments.ProfileFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.parse.*
 import java.io.File
@@ -31,39 +36,34 @@ class MainActivity : AppCompatActivity() {
         //2. a button to launch the camera
         //3. an imageview to show picture
         //4. a button to save and send the post to server
-        queryPosts()
 
-        findViewById<Button>(R.id.btn_submit).setOnClickListener(){
-            //send post to server without images
-            val description = findViewById<EditText>(R.id.et_description).text.toString()
-            val user = ParseUser.getCurrentUser()
-            if(photoFile != null){
-                submitPost(description, user, photoFile!!)
-            }else{
-                //print error
-                Log.e(TAG, "fail at taking picture")
-            }
-        }
-        findViewById<Button>(R.id.btn_takePicture).setOnClickListener(){
-            //launch camera to take picture
-            onLaunchCamera()
-        }
-        findViewById<BottomNavigationView>(R.id.action_home).setOnItemSelectedListener{
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener{
             item ->
+            var fragmentToShow: Fragment ?= null
             when(item.itemId){
                 R.id.action_home-> {
-                    //navigate to home screen
+                    //nevigate to home screen
+                    fragmentToShow = FeedFragment()
+
                 }
                 R.id.action_compose-> {
-                    //navigate to compose
+                    //nevigate to compose
+                    fragmentToShow = ComposeFragment()
                 }
                 R.id.action_profile-> {
-                    //navigate to profile
+                    //nevigate to profile
+                    fragmentToShow = ProfileFragment()
                 }
             }
-            //handle user interaction on the item
+            if(fragmentToShow != null){
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragmentToShow).commit()
+
+            }
             true
         }
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.action_home
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -81,83 +81,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onLaunchCamera() {
-        // create Intent to take a picture and return control to the calling application
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        // Create a File reference for future access
-        photoFile = getPhotoFileUri(photoFileName)
-
-        // wrap File object into a content provider
-        // required for API >= 24
-        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-        if (photoFile != null) {
-            val fileProvider: Uri =
-                FileProvider.getUriForFile(this, "com.codepath.fileprovider", photoFile!!)
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
-
-            // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-            // So as long as the result is not null, it's safe to use the intent.
-
-            // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-            // So as long as the result is not null, it's safe to use the intent.
-            if (intent.resolveActivity(packageManager) != null) {
-                // Start the image capture intent to take photo
-                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE)
-            }
-        }
-    }
-
-    fun getPhotoFileUri(fileName: String): File {
-        // Get safe storage directory for photos
-        // Use `getExternalFilesDir` on Context to access package-specific directories.
-        // This way, we don't need to request external read/write runtime permissions.
-        val mediaStorageDir =
-            File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG)
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
-            Log.d(TAG, "failed to create directory")
-        }
-
-        // Return the file target for the photo based on filename
-        return File(mediaStorageDir.path + File.separator + fileName)
-    }
-
-    fun submitPost(description: String, user: ParseUser, file: File) {
-        val post = Post()
-        post.setDescription(description)
-        post.setUser(user)
-        post.setImage(ParseFile(file))
-        post.saveInBackground{ exception->
-            if(exception != null){
-                Log.e(TAG, "error while saving post")
-                exception.printStackTrace()
-            }else{
-                Log.i(TAG, "successfully save post")
-            }
-        }
-    }
-
     //query for all posts in server
-    fun queryPosts() {
-        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
-        //find all post objects
-        query.include(Post.KEY_USER)
-        query.findInBackground(object: FindCallback<Post> {
-            override fun done(posts: MutableList<Post>?, e: ParseException?) {
-                if(e != null){
-                    Log.e(TAG, "error fetching post")
-                }else{
-                    if(posts != null){
-                        for(post in posts){
-                            Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser())
-                        }
-                    }
-                }
 
-            }
-        })
-    }
     companion object{
         const val TAG = "MainActivity"
     }
